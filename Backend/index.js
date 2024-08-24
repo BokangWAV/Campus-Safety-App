9a192135ef63234b264cf4b56ccb672b337e194a
DanielDanzo <sempapadaniel123@gmail.com>
2024-08-24 20:14:38 +0200

    changed the port

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 80;
+const port = 3000;
 
 app.use(cors());
 
 app.get('/',(req, res)=>{
+    console.log('We have an API');
     res.json({message: 'Exe Gents'});
 });
 
