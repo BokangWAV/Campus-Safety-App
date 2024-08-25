f65cf802581e81f7d182600185b4fd5c8eabcfd1
DanielDanzo <sempapadaniel123@gmail.com>
2024-08-25 17:25:27 +0200

    test google signIn

--- Backend/index.js
+++ Backend/index.js
@@ -3,12 +3,14 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000;;
+const port = process.env.PORT || 3000;
+const GooglesignInUser = require('modules/users.js');
 
 app.use(cors());
 
-app.get('/get',(req, res)=>{
+app.get('/login',(req, res)=>{
     console.log('We have an API');
+    GooglesignInUser();
     res.json({message: 'Exe Gents'});
 });
 
