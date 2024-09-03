61b82b5152fc49e9e8978735401b4da779489a8b
DanielDanzo <140892269+DanielDanzo@users.noreply.github.com>
2024-09-03 23:47:53 +0200

    Update init.js

--- Backend/modules/init.js
+++ Backend/modules/init.js
@@ -10,12 +10,12 @@ const { getStorage } = require('firebase/storage');
 //const dotenv = require('dotenv')
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-    authDomain: process.env.API_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.PROCESS_BUCKET,
+  apiKey: "AIzaSyCLDopG2959mh9Wtl3nDM0FAWZBNc3GGLo",
+    authDomain: "tdkus-fcf53.firebaseapp.com",
+    projectId: "tdkus-fcf53",
+    storageBucket: "tdkus-fcf53.appspot.com",
     messagingSenderId: "144411393779",
-    appId: process.env.APP_ID,
+    appId: "1:144411393779:web:54a4013e6da2a974b4c186",
     measurementId: "G-5BPYZE953B"
 };
 
