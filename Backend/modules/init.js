b075796cf9bbecffa1fd017f9fa27921b6247938
DanielDanzo <140892269+DanielDanzo@users.noreply.github.com>
2024-09-09 22:50:32 +0200

    Update init.js

--- Backend/modules/init.js
+++ Backend/modules/init.js
@@ -1,35 +1,12 @@
-const { initializeApp } = require('firebase/app')
-const { getFirestore } = require('firebase/firestore');
-const { getAuth, GoogleAuthProvider } = require('firebase/auth');
-const { getStorage } = require('firebase/storage');
+var admin = require("firebase-admin");
+
+var serviceAccount = require(process.env.CERT_JSON);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 
-// just configuring the dotenv to work here
-//require('dotenv').config();
-//import './node_modules/dotenv/config.js';
-//const dotenv = require('dotenv')
+const db = admin.firestore();
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCLDopG2959mh9Wtl3nDM0FAWZBNc3GGLo",
-  authDomain: "tdkus-fcf53.firebaseapp.com",
-  projectId: "tdkus-fcf53",
-  storageBucket: "tdkus-fcf53.appspot.com",
-  messagingSenderId: "144411393779",
-  appId: "1:144411393779:web:54a4013e6da2a974b4c186",
-  measurementId: "G-5BPYZE953B"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// Initialize Cloud Firestore and get a reference to the service
-const db = getFirestore(app);
-
-//create google instance
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
-
-//const storage = getStorage();
-
-// Initialize Realtime Database and get a reference to the service
-
-module.exports = { db, auth, provider, firebaseConfig , getAuth};
+module.exports = { db };
