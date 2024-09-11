645237c3dc9b641c342eb3fe542321ab0bd8b8c1
DanielDanzo <140892269+DanielDanzo@users.noreply.github.com>
2024-09-11 17:38:05 +0200

    Update init.js

--- Backend/modules/init.js
+++ Backend/modules/init.js
@@ -1,22 +1,13 @@
 var admin = require("firebase-admin");
 
-const serviceAccount = {
-  "type": process.env.CERT_TYPE,
-  "project_id": process.env.CERT_PROJECT_ID,
-  "private_key_id": process.env.CERT_PRIVATE_KEY_ID,
-  "private_key": process.env.CERT_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  "client_email": process.env.CERT_CLIENT_EMAIL,
-  "client_id": process.env.CERT_CLIENT_ID,
-  "auth_uri": process.env.CERT_AUTH_URI,
-  "token_uri": process.env.CERT_TOKEN_URI,
-  "auth_provider_x509_cert_url": process.env.CERT_AUTH_PROVIDER,
-  "client_x509_cert_url": process.env.CERT_CLIENT_CERT_URL,
-  "universe_domain": process.env.CERT_UNIVERSE_DOMAIN
-}
+
+const base64EncodedServiceAccount = process.env.BASE64_ENCODED_SERVICE_ACCOUNT;
+const decodedServiceAccount = Buffer.from(base64EncodedServiceAccount, 'base64').toString('utf-8');
+const credentials = JSON.parse(decodedServiceAccount);
 
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(credentials),
 });
 
 
