341c794c6d78b54607e8e6e98042fc848f37e787
Bokang <119689611+BokangWAV@users.noreply.github.com>
2024-09-17 15:16:10 +0200

    Update main.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -285,9 +285,9 @@ submit_btn.addEventListener("click",async ()=>{
 
 googleBtn.addEventListener('click', ()=>{
     GooglesignInUser();
-    if(await GooglesignInUser()){
+   // if(await GooglesignInUser()){
           window.location.href = "https://agreeable-forest-0b968ac03.5.azurestaticapps.net/dashboardtest.html"
-        }
+     //   }
   
 });
 
