96d729a4218d2e2efbbed43f49f82223f4248092
DanielDanzo <140892269+DanielDanzo@users.noreply.github.com>
2024-09-17 09:41:30 +0200

    Update report.js

--- Backend/modules/report.js
+++ Backend/modules/report.js
@@ -31,6 +31,7 @@ async function addReport(uid, report){
             }
         }).catch((error) => {
             //Do not do anything we just return an empty object
+            console.error("Error writing document: ", error);
         })
 
         const usersRef = db.collection('users').where("role", "==", "manager");
@@ -47,7 +48,7 @@ async function addReport(uid, report){
         await appendNotifications(idArray, 'added a new report', user2);
     })
     .catch((error) => {
-        //console.error("Error writing document: ", error);
+        console.error("Error writing document: ", error);
         added = false;
     });
 
