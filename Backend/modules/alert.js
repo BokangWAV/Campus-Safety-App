4f89a16005c8ffb1c6eb571be699298cd9c9213d
DanielDanzo <140892269+DanielDanzo@users.noreply.github.com>
2024-09-30 12:25:17 +0200

    Update alert.js

--- Backend/modules/alert.js
+++ Backend/modules/alert.js
@@ -1,71 +1,48 @@
-const {db, FieldValue} = require('./init.js');
 const { appendNotifications } = require('./notification.js');
-
-async function getAllAlerts(){
-    const usersRef = db.collection('alert');    // Get a reference to the articles collection
-
-    var result = [];
-    // First page of results
-    await usersRef.get().then(snapshot => {
-        if (!snapshot.empty) {
-            snapshot.forEach(doc =>{
-                result.push(doc.data());
-            })
-        }
-    });
-
-    return result;
-}
+const {db, FieldValue} = require('./init.js');
 
 
-async function addAlert(uid, alert){
-    let added = true;
+async function addReport(uid, report){
+    let added = true;     // Keeps track of whether or not we added the report
 
-    const reportsRef = db.collection('alert')
-    // Add a new document with a generated id.
+    const userRef = db.collection("reports");    //Stores a reference to the user
 
-    const response = await reportsRef.orderBy("alertID", "desc").get();
+    const response = await userRef.orderBy("reportID", "desc").get();
     var count = 0;
     if( response.docs.length > 0){
         count = Number(response.docs[0].data().alertID)
     }
     
     count = count + 1
-    //console.log(count)
 
-    await reportsRef.add({
-        alertDate: FieldValue.serverTimestamp(),
-        details: "EMERGENCY",
-        firstName: alert.firstName,
-        lastName: alert.lastName,
-        status: "processing",
-        lat: alert.lat,
-        lon: alert.lon,
-        age: alert.age,
-        race: alert.race,
-        gender: alert.gender,
-        phoneNumber: alert.phoneNumber,
-        alertID: count,
-        uid: uid
+    await userRef.add({
+        geoLocation: report.geoLocation,
+        description: report.description,
+        location: report.location,
+        urgencyLevel: report.urgencyLevel,
+        status: report.status,
+        timestamp: FieldValue.serverTimestamp(),
+        imageUrls: report.imageUrls,
+        videoUrls: report.videoUrls,
+        uid: uid,
+        reportID: count,
+        removed: false
     })
-    .then(async (docRef) => {
-        console.log("Alert sent")
-        var user = {};
+    .then(async () => {
+        var user2 = {};
 
         const q = db.collection('users').doc(uid);
 
         await q.get().then((doc) => {
             if (doc.exists) {
                 const response = doc.data();
-                user["firstName"] = response.firstName;
-                user["lastName"] = response.lastName;
-                user["profilePicture"] = response.profilePicture;
-                user["race"] = response.race;
-                user["age"] = response.age;
-                user["phoneNumber"] = response.phoneNumber
+                user2["firstName"] = response.firstName;
+                user2["lastName"] = response.lastName;
+                user2["profilePicture"] = response.profilePicture;
             }
         }).catch((error) => {
             //Do not do anything we just return an empty object
+            console.error("Error writing document: ", error);
         })
 
         const usersRef = db.collection('users').where("role", "==", "manager");
@@ -74,15 +51,14 @@ async function addAlert(uid, alert){
         await usersRef.get().then(snapshot => {
             if (!snapshot.empty) {
                 snapshot.forEach(doc =>{
-                    idArray.push(doc.id);
+                    if(doc.id != uid)idArray.push(doc.id);
                 })
             }
         });
-
-        appendNotifications(idArray, 'requires immediate attention', user, "alert", `${alert.lat} ${alert.lon}`, user.profilePicture);
+        appendNotifications(idArray, `${user2.firstName} ${user2.lastName} requires immediate attention`, user2, 'report', report.location,report.imageUrls );
     })
     .catch((error) => {
-        //console.error("Error adding document: ", error);
+        console.error("Error writing document: ", error);
         added = false;
     });
 
@@ -91,79 +67,8 @@ async function addAlert(uid, alert){
 
 
 
-async function deleteReport(reportID){
-    let deleted = true;
-    const usersRef = await db.collection('alert').where("alertID", "==", Number(reportID)).get();
-
-    const doc = usersRef.docs[0];
-
-    const articlesRef2 = db.collection('alert')
-
-    await articlesRef2.doc(doc.id).delete()
-    .then(() => {
-        //console.log("Document successfully deleted!");
-    }).catch((error) => {
-        //console.error("Error removing document: ", error);
-        deleted = false;
-    });
-
-    return deleted;
-    
-}
-
-
-async function updateViewAlert(reportID){
-
-    let added = true;
-    const articlesRef = await db.collection('alert').where("uid", "==", reportID).get();
-
-    const doc = articlesRef.docs[0];
-
-    const articlesRef2 = db.collection('alert')
-
-    await articlesRef2.doc(doc.id).update({
-        status: "ASSISTED"
-    })
-    .then(() => {
-        //console.log("Document successfully updated!");
-    })
-    .catch((error) => {
-        // The document probably doesn't exist.
-        //console.error("Error updating document: ", error);
-        added = false;
-    });
-
-    return added;
-}
-
-
-async function managerViewAlert(reportID){
-
-    let added = true;
-    const articlesRef = await db.collection('alert').where("alertID", "==", Number(reportID)).get();
-
-    const doc = articlesRef.docs[0];
-
-    const articlesRef2 = db.collection('alert')
-
-    await articlesRef2.doc(doc.id).update({
-        status: "ASSISTED"
-    })
-    .then(() => {
-        //console.log("Document successfully updated!");
-    })
-    .catch((error) => {
-        // The document probably doesn't exist.
-        //console.error("Error updating document: ", error);
-        added = false;
-    });
-
-    return added;
-}
-
-
-async function getUserAlerts(uid){
-    const usersRef = db.collection('alert').where("uid", "==", uid);    // Get a reference to the articles collection
+async function getAllReports(){
+    const usersRef = db.collection('reports');    // Get a reference to the reports collection
 
     var result = [];
     // First page of results
@@ -180,11 +85,53 @@ async function getUserAlerts(uid){
 
 
 
+async function getUserReport(uid){
+    const usersRef = db.collection('reports').where("uid", "==", uid);    // Get a reference to the articles collection
+
+    var result = [];
+    // First page of results
+    await usersRef.get().then(snapshot => {
+        if (!snapshot.empty) {
+            snapshot.forEach(doc =>{
+                result.push(doc.data());
+            })
+        }
+    });
+
+    return result;
+}
+
+
+
+async function removeReport(reportID){
+    let removal = false;
+    console.log(reportID);
+    const usersRef = await db.collection('reports').where("reportID", "==", Number(reportID)).get();    // Get a reference to the articles collection
+
+    const doc = usersRef.docs[0];
+    console.log(usersRef.docs[0].id)
+    console.log(doc)
+
+    const articlesRef2 = db.collection('reports')
+
+    await articlesRef2.doc(doc.id).update({
+        removed: true
+    })
+    .then(() => {
+        removal = true
+       // console.log("Document successfully updated!");
+    })
+    .catch((error) => {
+        console.error(error)
+        // The document probably doesn't exist.
+        //console.error("Error updating document: ", error);
+    });
+
+    return removal;
+}
 
 
 
 
 
-
-
-module.exports = { getAllAlerts, addAlert, deleteReport, updateViewAlert, getUserAlerts, managerViewAlert }
+module.exports = { addReport, getAllReports, getUserReport, removeReport  }
