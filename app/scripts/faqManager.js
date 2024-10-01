d4c044b661e08f025ac641503a77c25740481be5
Njabulo Mahlangu <160496619+NjabuloSM@users.noreply.github.com>
2024-10-01 04:59:46 +0200

    Update faqManager.js

--- app/scripts/faqManager.js
+++ app/scripts/faqManager.js
@@ -30,6 +30,7 @@ document.addEventListener("DOMContentLoaded", async function(){
                 card.style.margin = "20px";
 
                 let par = document.createElement("p");
+                par.id = "myQuestion";
                 par.textContent = question.question;
                 par.style.alignSelf = "center";
                 par.style.fontWeight = "600";
@@ -69,7 +70,7 @@ document.addEventListener("DOMContentLoaded", async function(){
                 let textArea = document.createElement("textarea");
                 textArea.id = "myAnswer"
                 textArea.style.minWidth = "90%";
-                textArea.style.minHeight = "3em";
+                textArea.style.minHeight = "5em";
 
                 card.appendChild(headQ);
                 card.appendChild(par);
@@ -129,43 +130,103 @@ document.addEventListener("DOMContentLoaded", async function(){
             }
         })
 
-        document.querySelector("#preloader").style.opacity = "0";
+        document.querySelector("#preloader").remove();
         document.querySelector(".main-container").style.opacity = "1";    
         
-        document.querySelector("#delete").addEventListener("click", () =>{
-            const thisDiv = this.parentElement;
-            const thisID = this.dataset.faqid;
+        const deleteBtns = document.querySelectorAll("#delete")
+        
+        deleteBtns.forEach(button => {
+            button.addEventListener("click", (event) =>{
+                const thisDiv = event.target.parentElement;
+                const thisID = event.target.dataset.faqid;
 
-            fetch(`https://sdp-campus-safety.azurewebsites.net/FAQ/${thisID}`, {
-                method: 'DELETE',
-                headers: {'Content-Type': 'application/json'}
-            }).then(response => {
-                if(response.ok){
-                    thisDiv.remove();
-                    alert("FAQ successfully deleted.");
-                }else{
-                    console.error("Error:", response.statusText);
-                    alert("An error has occurred");
-                }
-                
-            }).catch(error => console.error("Error:", error));
+                fetch(`https://sdp-campus-safety.azurewebsites.net/FAQ/${thisID}`, {
+                    method: 'DELETE',
+                    headers: {'Content-Type': 'application/json'}
+                }).then(response => {
+                    if(response.ok){
+                        console.log("FAQ ID:", event.target.dataset.faqid);
+                        thisDiv.remove();
+                        alert("FAQ successfully deleted.");
+                    }else{
+                        console.error("Error:", response.statusText);
+                        alert("An error has occurred");
+                    }
+                    
+                }).catch(error => console.error("Error:", error));
+            })
         })
 
-        document.querySelector("#approve").addEventListener("click", () =>{
-            const myDiv =  this.parentElement;
+        const approveBtns = document.querySelectorAll("#approve")
+        
+        approveBtns.forEach(approve => {
+            approve.addEventListener("click", (event) =>{
+            const container =  event.target.parentElement;
+            const myDiv = container.parentElement;
             const text = myDiv.querySelector("#myAnswer").value;
-            const faq_id = this.dataset.faqid;  
+            const question = myDiv.querySelector("#myQuestion").textContent;
+            const faq_id = event.target.dataset.faqid;  
             const data = {
                 answer: text
             }
+
             fetch(`https://sdp-campus-safety.azurewebsites.net/FAQ/${faq_id}`, {
                 method: 'PUT',
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify(data)
             }).then(response => {
                 if(response.ok){
-                    alert("FAQ updated");
-                    myDiv.remove()
+                    alert("FAQ updated!");
+
+                let card = document.createElement("div");
+                card.className = "dashboard-card";
+                card.style.margin = "20px";
+
+                let headQ = document.createElement("h3");
+                let headA = document.createElement("h3");
+
+                headQ.textContent = "Question";
+                headA.textContent = "Answer";
+
+                let par = document.createElement("p");
+                par.id = "myQuestion";
+                par.textContent = question;
+                par.style.alignSelf = "center";
+                par.style.fontWeight = "600";
+                par.style.color = "black"
+
+                let answerPar = document.createElement("p");
+                answerPar.textContent = text;
+                answerPar.style.alignSelf = "center";
+                answerPar.style.fontWeight = "600";
+                answerPar.style.color = "black"
+
+                let buttonContainer = document.createElement("div");
+                buttonContainer.style.display = "flex";
+                buttonContainer.style.flexWrap = "wrap";
+
+                let deleteBtn = document.createElement("button");
+                deleteBtn.id = "delete";
+                deleteBtn.dataset.faqid = question.FAQID;
+                deleteBtn.textContent = "Delete";
+
+                deleteBtn.style.border = "none";
+                deleteBtn.style.border = "none";
+                deleteBtn.style.backgroundColor = "red";
+                deleteBtn.style.padding = "20px";
+                deleteBtn.style.margin = "10px";
+                deleteBtn.style.color = "white";
+
+                card.appendChild(headQ)
+                card.appendChild(par);
+                card.appendChild(headA);
+                card.appendChild(answerPar);
+                buttonContainer.appendChild(deleteBtn);
+                card.appendChild(buttonContainer);
+
+                document.querySelector(".approvedCards").appendChild(card);
+
+                    myDiv.remove();
                     return response.json();
                 }else{
                     alert("Something went wrong!");
@@ -176,7 +237,25 @@ document.addEventListener("DOMContentLoaded", async function(){
             }).catch(error => {
                 console.error("Error:", error);
             })
+
+
+            fetch(`https://sdp-campus-safety.azurewebsites.net/FAQ/publish/${faq_id}`, {
+                method: 'PUT',
+                headers: {'Content-Type': 'application/json'}
+            }).then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Success:', data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
         })
+    })
 
     } catch (error) {
         console.error(error);
