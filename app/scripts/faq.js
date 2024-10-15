9f84ecf28581feb5bcd50f02ccb422ac22f11ca4
DanielDanzo <140892269+DanielDanzo@users.noreply.github.com>
2024-10-15 13:35:47 +0200

    Update faq.js

--- app/scripts/faq.js
+++ app/scripts/faq.js
@@ -59,6 +59,46 @@ async function fetchData(url) {
 }
 
 document.addEventListener("DOMContentLoaded", async function() {
+    let textArea = document.createElement("textarea");
+    textArea.id = "faq-textarea";
+    textArea.name = "faq-textarea";
+    textArea.style.resize = 'none';
+
+    let btn = document.createElement("button");
+    btn.id = "submission";
+    btn.type = "button";
+    btn.textContent = "Submit FAQ";
+    
+    const faqQuestionContainer = document.querySelector(".faq-question");
+    faqQuestionContainer.appendChild(textArea);
+    faqQuestionContainer.appendChild(btn);
+
+    btn.addEventListener("click", () => {
+        let value = textArea.value;
+
+        const data = {
+            question: value
+        }
+        
+        disableButton();
+        postFAQ(data, userUID)
+            .then(() => {
+                textArea.value = "";
+                disableButton();
+            })
+            .catch(error => {
+                console.error("Error posting FAQ:", error);
+                alert("Failed to post article. Please try again.");
+            })
+            .finally(() => {
+                enableButton();
+            });
+
+        textArea.value = "";
+    });
+
+
+
     try {
         const list = await fetchData('https://sdp-campus-safety.azurewebsites.net/FAQs');
 
@@ -87,42 +127,7 @@ document.addEventListener("DOMContentLoaded", async function() {
     }
 
     
-        let textArea = document.createElement("textarea");
-        textArea.id = "faq-textarea";
-        textArea.name = "faq-textarea";
-
-        let btn = document.createElement("button");
-        btn.id = "submission";
-        btn.type = "button";
-        btn.textContent = "Submit FAQ";
         
-        const faqQuestionContainer = document.querySelector(".faq-question");
-        faqQuestionContainer.appendChild(textArea);
-        faqQuestionContainer.appendChild(btn);
-
-        btn.addEventListener("click", () => {
-            let value = textArea.value;
-
-            const data = {
-                question: value
-            }
-            
-            disableButton();
-            postFAQ(data, userUID)
-                .then(() => {
-                    textArea.value = "";
-                    disableButton();
-                })
-                .catch(error => {
-                    console.error("Error posting FAQ:", error);
-                    alert("Failed to post article. Please try again.");
-                })
-                .finally(() => {
-                    enableButton();
-                });
-
-            textArea.value = "";
-        });
 });
 function addCard(question, answer, index){
     document.querySelector(".faq-list").innerHTML += `<div id="card"><i>${question}</i><div class="question" id=faq${index}><p>${answer}</p></div></div>`
